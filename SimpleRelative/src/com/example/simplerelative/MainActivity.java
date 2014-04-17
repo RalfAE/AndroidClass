412c23cb97a9966c91372f6775ca814e4d75c5c0
RalfAE <RalfAE@mail.com>
2014-04-17 22:06:22 +0800

    relative add date spinner

--- SimpleRelative/src/com/example/simplerelative/MainActivity.java
+++ SimpleRelative/src/com/example/simplerelative/MainActivity.java
@@ -54,7 +54,8 @@ public class MainActivity extends ActionBarActivity {
 		public PlaceholderFragment() {
 		}
 
-		private Spinner spinner = null;
+		private Spinner spinnerMonth = null;
+		private Spinner spinnerDate = null;
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -62,13 +63,23 @@ public class MainActivity extends ActionBarActivity {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
 			
-			spinner = (Spinner) rootView.findViewById(R.id.monthSpinner);
+			spinnerMonth = (Spinner) rootView.findViewById(R.id.monthSpinner);
+			spinnerDate = (Spinner) rootView.findViewById(R.id.dateSpinner);
 			
-			String[] list = getResources().getStringArray(R.array.Month);
+			String[] monthList = getResources().getStringArray(R.array.Month);
 			
-			ArrayAdapter<String> sa = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, list);
+			ArrayAdapter<String> saMonth = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, monthList);
 
-			spinner.setAdapter(sa);
+			spinnerMonth.setAdapter(saMonth);
+			
+//			String[] dateList = new String[31];
+//			for (int i = 1; i <= 31; i++) {
+//				dateList[i] = String.valueOf(i);
+//			}
+//			
+//			ArrayAdapter<String> saDate = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, dateList);
+//
+//			spinnerDate.setAdapter(saDate);
 			
 			return rootView;
 		}
