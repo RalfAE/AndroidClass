cf68cf26f3c47537bb3c819c19f4331602073ece
RalfAE <RalfAE@mail.com>
2014-04-21 22:06:38 +0800

    simple ui add SharedPreferences

--- SimpleUI/src/com/example/simpleui/MainActivity.java
+++ SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.simpleui;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.content.IntentSender.SendIntentException;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -18,50 +19,47 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+		public PlaceholderFragment() {
+		}
 
 		private void sendMsg() {
 			String text = textField.getText().toString();
@@ -72,46 +70,72 @@ public class MainActivity extends ActionBarActivity {
 			textField.getText().clear();
 		}
 
-        Button btn;
+		Button btn;
 		EditText textField;
 		CheckBox checkBox;
-        
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            btn = (Button) rootView.findViewById(R.id.button2);
-            textField = (EditText) rootView.findViewById(R.id.editText1);
-            checkBox = (CheckBox) rootView.findViewById(R.id.checkBox1);
-            
-            btn.setOnClickListener(new OnClickListener() {
-				
+
+		SharedPreferences sp;
+		Editor editor;
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			btn = (Button) rootView.findViewById(R.id.button2);
+			textField = (EditText) rootView.findViewById(R.id.editText1);
+			checkBox = (CheckBox) rootView.findViewById(R.id.checkBox1);
+
+			sp = getActivity().getSharedPreferences("settings",
+					Context.MODE_PRIVATE);
+			editor = sp.edit();
+
+			btn.setOnClickListener(new OnClickListener() {
+
 				@Override
 				public void onClick(View v) {
-					sendMsg();		
+					sendMsg();
 				}
 			});
 
-            
-            textField.setOnKeyListener(new OnKeyListener() {
-				
+			checkBox.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					editor.putBoolean("checked", checkBox.isChecked());
+					editor.commit();
+				}
+			});
+
+			textField.setOnKeyListener(new OnKeyListener() {
+
 				@Override
 				public boolean onKey(View v, int keyCode, KeyEvent event) {
-					Log.d("androidClass", "keyCode:" + keyCode + ", event: " + event.getAction());
-					
-					if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+					Log.d("androidClass", "keyCode:" + keyCode + ", event: "
+							+ event.getAction());
+
+					if (event.getAction() == KeyEvent.ACTION_DOWN
+							&& keyCode == KeyEvent.KEYCODE_ENTER) {
 						sendMsg();
 						return true;
+					} else {
+
+						editor.putString("text", textField.getText().toString());
+						editor.commit();
+
 					}
 					return false;
 				}
 			});
-            
-            return rootView;
-        }
-    }
-    
-    public void btnClick(View view) {
-    	Log.d("androidClass", "clicked");
-    }
+
+			textField.setText(sp.getString("text", "Hello World"));
+			checkBox.setChecked(sp.getBoolean("checked", false));
+
+			return rootView;
+		}
+	}
+
+	public void btnClick(View view) {
+		Log.d("androidClass", "clicked");
+	}
 }
