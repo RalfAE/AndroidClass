97f38b12a6bbc74d080b07055ade16dc68be5936
RalfAE <RalfAE@mail.com>
2014-05-05 20:04:16 +0800

    get intent photo and show pic

--- TakePhoto/src/com/example/takephoto/MainActivity.java
+++ TakePhoto/src/com/example/takephoto/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.takephoto;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -11,8 +13,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+@SuppressLint("ValidFragment")
 public class MainActivity extends ActionBarActivity {
+
+	private static final int REQUEST_CODE = 2266;
+	ImageView imageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +50,47 @@ public class MainActivity extends ActionBarActivity {
 			Log.d("Hit Menu", "HIT!!!");
 			Intent intent = new Intent();
 			intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-			startActivity(intent);
+			// 改用會回傳結果的方法
+			// startActivity(intent);
+			startActivityForResult(intent, REQUEST_CODE);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+
+		// 鄰斷是由take photo呼叫的result
+		if (requestCode == REQUEST_CODE) {
+
+			if (resultCode == RESULT_OK) {
+				Log.d("Debug", "OK!");
+				Bitmap bitmap = intent.getParcelableExtra("data");
+				imageView.setImageBitmap(bitmap);
+				
+			} else if (resultCode == RESULT_CANCELED) {
+				Log.d("Debug", "Cancel!");
+
+				imageView.setImageBitmap(null);
+				
+			} else {
+				Log.d("Debug", "other!");
+				
+			}
+
+		}
+
+	}
+	
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	public class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
@@ -62,6 +100,7 @@ public class MainActivity extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
+			imageView = (ImageView) rootView.findViewById(R.id.imageView1);
 			return rootView;
 		}
 	}
